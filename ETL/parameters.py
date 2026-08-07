f35a71c30bf9e2de7a48db486c899e19ae6d7368
Christina Francis <CFranc22@jhu.edu>
2026-08-07 13:56:24 -0400

    Created a central source of truth for all universal parameters used throughout ETL scripts
    Created parameters.py

---
+++ ETL/parameters.py
@@ -0,0 +1,5 @@
+"""
+parameters.py contains the default values for all parameters used across ETL scripts.
+"""
+
+NOAA_ONI_URL = "https://www.cpc.ncep.noaa.gov/products/analysis_monitoring/enso/oni/v6/"
