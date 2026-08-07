6eecd0a14562ce8e272cc63458c91e439632b141
Christina Francis <CFranc22@jhu.edu>
2026-08-07 14:26:30 -0400

    Updated parameters.py
    Added parameters for widely used storage/config defaults and hard-coded literals

--- ETL/parameters.py
+++ ETL/parameters.py
@@ -3,3 +3,27 @@ parameters.py contains the default values for all parameters used across ETL scr
 """
 
 NOAA_ONI_URL = "https://www.cpc.ncep.noaa.gov/products/analysis_monitoring/enso/oni/v6/"
+
+R2_BUCKET = "noaa-enso-scraper"
+R2_PREFIX = "data"
+PARQUET_FILENAME = "enhanced_oni_latest.parquet"
+NETCDF_FILENAME = "enhanced_oni_latest.nc"
+FORMAT_PARQUET = "parquet"
+FORMAT_NETCDF = "netcdf"
+
+DIM_YEAR = "year"
+DIM_SEASON = "season"
+DATASET_VAR_ONI = "ONI"
+DATASET_VAR_ENSO = "ENSO"
+ENSO_PHASE_LA_NINA = "LaNina"
+ENSO_PHASE_EL_NINO = "ElNino"
+ENSO_PHASE_NEUTRAL = "Neutral"
+ENSO_PHASE_UNKNOWN = "Unknown"
+
+FIELD_INTENSITY = "intensity"
+FIELD_PHASE_DURATION = "phase_duration"
+FIELD_RATE_OF_CHANGE = "rate_of_change"
+FIELD_PERCENTILE = "percentile"
+ENSO_PHASE_LABEL_LA_NINA = "La Niña"
+ENSO_PHASE_LABEL_EL_NINO = "El Niño"
+ENSO_PHASE_LABEL_NEUTRAL = "Neutral"
